d7daabe60bf621e407961b015c3fe1159062bf65
ShuqiYang <49054767+shuqiyang@users.noreply.github.com>
2019-11-13 15:40:01 +0800

    Create installconcourse.sh

---
+++ installconcourse.sh
@@ -0,0 +1,4 @@
+sudo curl -s  -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
+sudo chmod +x /usr/local/bin/docker-compose
+sudo wget -q https://concourse-ci.org/docker-compose.yml
+sudo docker-compose up -d
