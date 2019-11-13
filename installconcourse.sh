70bfec4d7091a807e5f68d141b1e2bf1782b83f9
ShuqiYang <49054767+shuqiyang@users.noreply.github.com>
2019-11-13 15:50:52 +0800

    Update installconcourse.sh

--- installconcourse.sh
+++ installconcourse.sh
@@ -1,5 +1,5 @@
 echo "hello, we will try to install concourse!"
-sudo curl -s  -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
-sudo wget -q https://concourse-ci.org/docker-compose.yml
+sudo wget https://concourse-ci.org/docker-compose.yml
 sudo docker-compose up -d
